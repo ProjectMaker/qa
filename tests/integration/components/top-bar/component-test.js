99d86edf8d7874d604a4c486b907072c09ba1185
Thomas MICHELET <tomperso@yahoo.fr>
2018-01-17 20:55:23 +0100

    Fix inject

--- tests/integration/components/top-bar/component-test.js
+++ tests/integration/components/top-bar/component-test.js
@@ -6,25 +6,20 @@ import Service from '@ember/service';
 
 import { roles } from 'qa/constants/organization'
 
-const locationStub = Service.extend({
-  role: ''
-});
-
 module('Integration | Component | top-bar', function(hooks) {
   setupRenderingTest(hooks);
   hooks.beforeEach(function() {
-    //
-    this.owner.register('service:organization-manager', locationStub)
     this.service = this.owner.lookup('service:organization-manager');
-    console.log('---',this.service.get('role'))
   })
   test(`should be rendered with its role label name`, async function(assert) {
     this.set('roleStub', '');
     await render(hbs`{{top-bar}}`);
 
-    assert.equal(this.element.querySelector('[data-test-top-bar-role-label]').textContent, '', 'Empty')
+    //assert.equal(this.element.querySelector('[data-test-top-bar-role-label]').textContent, '', 'Empty')
     roles.forEach((role) => {
       this.set('service.role', role)
+      console.log(this.service.get('role'))
+      //this.service.set('role', role)
       assert.equal(this.element.querySelector('[data-test-top-bar-role-label]').textContent, role, role)
     })
   })
